6b057dfe6cf013d6882ebd7fd54d81856bbebb64
aroyy007 <223016412@eastdelta.edu.bd>
2024-11-08 09:22:38 +0600

    First Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Victim from './pages/Victim';
+import Volunteer from './pages/Volunteer';
+import Dashboard from './pages/Dashboard';
+import VictimPage from './pages/VictimPage';
+import VolunteerPage from './pages/VolunteerPage';
+
+
 function App() {
   return (
-    <>
-  
-    </>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/victim" element={<Victim />} />
+        <Route path="/volunteer" element={<Volunteer />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/victim-page" element={<VictimPage />} />
+        <Route path="/volunteer-page" element={<VolunteerPage />} />
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/VictimPage.tsx
@@ -0,0 +1,9 @@
+function VictimPage() {
+    return (
+        <div>
+            <h1>Victim Page</h1>
+        </div>
+    )
+}
+
+export default VictimPage;
